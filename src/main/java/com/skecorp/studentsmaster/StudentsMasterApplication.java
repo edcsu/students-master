ab8a31dbf728814994f4abe68d2c834cc2da3919
Ssewannonda Keith Edwin <skeith696@gmail.com>
2022-11-08 00:39:51 +0300

    feat: add initial endpoint

--- src/main/java/com/skecorp/studentsmaster/StudentsMasterApplication.java
+++ src/main/java/com/skecorp/studentsmaster/StudentsMasterApplication.java
@@ -2,12 +2,39 @@ package com.skecorp.studentsmaster;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class StudentsMasterApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StudentsMasterApplication.class, args);
+    }
+
+    @GetMapping
+    Customer getCustomer() {
+        return new Customer(1L, "John Doe");
+    }
+
+    class Customer {
+        private final Long id;
+        private  final  String name;
+
+        Customer(Long id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+
+        public String getName() {
+            return name;
+        }
+
+        public Long getId() {
+            return id;
+        }
     }
 
 }
