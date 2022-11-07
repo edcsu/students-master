93ea8ef5a7f79d7b65859f79a5c8bb8e29bde628
Ssewannonda Keith Edwin <skeith696@gmail.com>
2022-11-08 00:20:49 +0300

    init: initial config

---
+++ src/main/java/com/skecorp/studentsmaster/StudentsMasterApplication.java
@@ -0,0 +1,13 @@
+package com.skecorp.studentsmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentsMasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skecorp/studentsmaster/StudentsMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skecorp.studentsmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
