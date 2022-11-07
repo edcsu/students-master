50dfa6991710831881039e3a0b7adb309db416e7
Ssewannonda Keith Edwin <skeith696@gmail.com>
2022-11-08 02:14:53 +0300

    refactor: set primary interface implementation

--- src/main/java/com/skecorp/studentsmaster/customer/CustomerRepository.java
+++ src/main/java/com/skecorp/studentsmaster/customer/CustomerRepository.java
@@ -1,5 +1,6 @@
 package com.skecorp.studentsmaster.customer;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
@@ -7,6 +8,7 @@ import java.util.Collections;
 import java.util.List;
 
 @Component
+@Primary
 public class CustomerRepository implements CustomerRepo {
     @Override
     public List<Customer> getCustomers() {
