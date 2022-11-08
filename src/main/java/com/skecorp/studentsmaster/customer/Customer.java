1fc8b1b232aa52bd9fed96ff68020a369012806c
Ssewannonda Keith Edwin <skeith696@gmail.com>
2022-11-08 20:48:12 +0300

    feat: add password property

--- src/main/java/com/skecorp/studentsmaster/customer/Customer.java
+++ src/main/java/com/skecorp/studentsmaster/customer/Customer.java
@@ -1,20 +1,32 @@
 package com.skecorp.studentsmaster.customer;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Customer {
     private final Long id;
     private  final  String name;
 
-    Customer(Long id, String name) {
+    private  final  String password;
+
+    Customer(Long id, String name, String password) {
         this.id = id;
         this.name = name;
+        this.password = password;
     }
 
     public String getName() {
         return name;
     }
 
+    @JsonProperty("customer_id")
     public Long getId() {
         return id;
+    }
+
+    @JsonIgnore
+    public String getPassword() {
+        return password;
     }
 
     @Override
@@ -22,6 +34,7 @@ public class Customer {
         return "Customer{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
                 '}';
     }
 }
--- src/main/java/com/skecorp/studentsmaster/customer/CustomerFakeRepository.java
+++ src/main/java/com/skecorp/studentsmaster/customer/CustomerFakeRepository.java
@@ -1,8 +1,5 @@
 package com.skecorp.studentsmaster.customer;
 
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
-
 import java.util.Arrays;
 import java.util.List;
 
@@ -10,9 +7,9 @@ public class CustomerFakeRepository implements CustomerRepo {
     @Override
     public List<Customer> getCustomers() {
         return Arrays.asList(
-                 new Customer(1L, "John Doe"),
-                 new Customer(2L, "Jane Doe"),
-                 new Customer(3L, "Antony De Beek")
+                 new Customer(1L, "John Doe", "password"),
+                 new Customer(2L, "Jane Doe", "password"),
+                 new Customer(3L, "Antony De Beek", "password")
         );
     }
 }
