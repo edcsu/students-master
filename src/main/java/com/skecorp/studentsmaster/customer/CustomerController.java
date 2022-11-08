10862fc6e3c6f1a1199a4c50eb25437acf187239
Ssewannonda Keith Edwin <skeith696@gmail.com>
2022-11-08 21:34:08 +0300

    feat: add post, edit and delete endpoints

--- src/main/java/com/skecorp/studentsmaster/customer/CustomerController.java
+++ src/main/java/com/skecorp/studentsmaster/customer/CustomerController.java
@@ -1,7 +1,6 @@
 package com.skecorp.studentsmaster.customer;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,5 +16,22 @@ public class CustomerController {
     @GetMapping
     List<Customer> getCustomer() {
         return customerService.getCustomers();
+    }
+
+    @PostMapping
+    void createCustomer(@RequestBody Customer customer){
+        System.out.println("Post request...");
+        System.out.println(customer);
+    }
+
+    @PutMapping
+    void updateCustomer(@RequestBody Customer customer){
+        System.out.println("Update request...");
+        System.out.println(customer);
+    }
+
+    @DeleteMapping(path = "{customerId}")
+    void deleteCustomer(@PathVariable("customerId") Long id){
+        System.out.println("Delete request for customer with id: " + id);
     }
 }
