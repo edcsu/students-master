6830c695a2a66d4fe688594a6d63179554d3c378
Ssewannonda Keith Edwin <skeith696@gmail.com>
2022-11-10 01:15:07 +0300

    feat: add open feign API client

--- src/main/java/com/skecorp/studentsmaster/StudentsMasterApplication.java
+++ src/main/java/com/skecorp/studentsmaster/StudentsMasterApplication.java
@@ -2,10 +2,12 @@ package com.skecorp.studentsmaster;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableFeignClients
 public class StudentsMasterApplication {
 
     public static void main(String[] args) {
