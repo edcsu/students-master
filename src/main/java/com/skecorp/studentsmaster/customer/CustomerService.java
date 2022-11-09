01899741455e2a2091189704d2944a73848a3c9d
Ssewannonda Keith Edwin <skeith696@gmail.com>
2022-11-10 01:34:08 +0300

    feat: add basic logging

--- src/main/java/com/skecorp/studentsmaster/customer/CustomerService.java
+++ src/main/java/com/skecorp/studentsmaster/customer/CustomerService.java
@@ -1,6 +1,8 @@
 package com.skecorp.studentsmaster.customer;
 
 import com.skecorp.studentsmaster.exception.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,16 +13,24 @@ public class CustomerService {
 
     private final CustomerRepository customerRepository;
 
+    private final static Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
+
     public CustomerService(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
     }
 
     List<Customer> getCustomers() {
+        LOGGER.info("getCustomers was called");
         return customerRepository.findAll();
     }
 
     Customer getCustomer( Long id) {
+        LOGGER.info("getCustomers was called for id {}", id);
         return customerRepository.findById(id)
-                .orElseThrow(() -> new NotFoundException("Customer with id " + id + " not found"));
+                .orElseThrow(() -> {
+                    NotFoundException notFoundException = new NotFoundException("Customer with id " + id + " not found");
+                    LOGGER.error("error in getCustomer {}", id, notFoundException);
+                    return notFoundException;
+                });
     }
 }
