005e5368ba2dd557a367e3ab77b5b67443645549
Ssewannonda Keith Edwin <skeith696@gmail.com>
2022-11-10 02:43:38 +0300

    feat: add profiles

--- src/main/java/com/skecorp/studentsmaster/customer/CustomerConfiguration.java
+++ src/main/java/com/skecorp/studentsmaster/customer/CustomerConfiguration.java
@@ -1,5 +1,6 @@
 package com.skecorp.studentsmaster.customer;
 
+import com.skecorp.studentsmaster.infoapp.InfoApp;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
@@ -11,10 +12,15 @@ public class CustomerConfiguration {
     @Value("${app.useFakeCustomerRepo:false}")
     private Boolean useFakeCustomerRepo;
 
+    @Value("${info.app.name}")
+    private String appName;
+
     @Bean
-    CommandLineRunner commandLineRunner() {
+    CommandLineRunner commandLineRunner(InfoApp infoApp) {
         return  args -> {
             System.out.println("Command  line runner hoooray!");
+            System.out.println("App name: " + appName);
+            System.out.println(infoApp);
         };
     }
 
